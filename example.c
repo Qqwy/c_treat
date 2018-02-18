b6233cbca2a177e82e4a69ece8f87c63b4fd2101
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-02-18 01:17:45 +0100

    WIP Trait system. Works, but very boilerplate-y

--- example.c
+++ example.c
@@ -1,12 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 #include "hash_map/hash_map.h"
 
-typedef struct {} Type;
+typedef struct {
+  size_t size;
+  void *(*ctor)(void *self, va_list *arguments);
+  void *(*dtor)(void *self);
+} Type;
 
 typedef struct {
-  void *typeref;
+  void *type;
 } TypeRef;
 
 typedef struct {
@@ -21,7 +26,7 @@ struct {
   .impl_for = &Trait_impl_for
 };
 
-void *Trait_impl_for(void const *_trait, Type * type) {
+void *Trait_impl_for(void const *_trait, Type *type) {
   TraitType trait = *(TraitType*)(_trait);
   return HashMap_lookup(trait.implementations, type);
 }
@@ -43,7 +48,7 @@ typedef struct {
 
 void Inspect_inspect(void *_obj) {
   TypeRef * obj = (TypeRef *) _obj;
-  void *_res = Trait.impl_for(&Inspect, obj->typeref);
+  void *_res = Trait.impl_for(&Inspect, obj->type);
   InspectTraitImplementation *res = _res;
   /* void (*res)(void *) = _res; */
   res->inspect(obj);
@@ -55,20 +60,51 @@ typedef struct {
 } TChar;
 
 TChar *Char_new(char character);
+void *Char_ctor(void *self, va_list *args);
+void *Char_dtor(void *self);
 
-/* Type Char = {}; */
 struct {
   Type type;
   TChar *(*new)(char character);
 } Char = {
-  .type = {},
+  .type.size = sizeof(TChar),
+  .type.ctor = &Char_ctor,
+  .type.dtor = &Char_dtor,
+
   .new = &Char_new
 };
 
+void *Char_ctor(void *_self, va_list *args) {
+  TChar *self = _self;
+  char character = va_arg(*args, int);
+  self->character = character;
+  return self;
+}
+
+void *Char_dtor(void *_self) {
+  TChar *self = _self;
+  // no-op
+  return self;
+}
+
+
+void *new(const void * _type, ...) {
+  const Type *type = _type;
+  void *obj = calloc(1, type->size);
+  /* assert(obj); */
+
+  *(const Type **) obj = type;
+  if(type->ctor) {
+    va_list args;
+    va_start(args, _type);
+    obj = type->ctor(obj, &args);
+    va_end(args);
+  }
+  return obj;
+}
 
 TChar *Char_new(char character) {
-  TChar *obj = calloc(1, sizeof(TChar));
-  obj->typeref.typeref = &Char;
+  TChar *obj = new(&Char, character);
   obj->character = character;
 
   return obj;
@@ -83,9 +119,63 @@ InspectTraitImplementation InspectTCharImplementation = {
   .inspect = &Inspect_TChar_inspect
 };
 
+
+typedef struct {
+  TypeRef typeref;
+  int integer;
+} TInteger;
+
+TInteger *Integer_new(int integer);
+void *Integer_ctor(void *self, va_list *args);
+void *Integer_dtor(void *self);
+
+struct {
+  Type type;
+  TInteger *(*new)(int integer);
+} Integer = {
+  .type.size = sizeof(TInteger),
+  .type.ctor = &Integer_ctor,
+  .type.dtor = &Integer_dtor,
+
+  .new = &Integer_new
+};
+
+void *Integer_ctor(void *_self, va_list *args) {
+  TInteger *self = _self;
+  int integer = va_arg(*args, int);
+  self->integer = integer;
+  return self;
+}
+
+void *Integer_dtor(void *_self) {
+  TInteger *self = _self;
+  // no-op
+  return self;
+}
+
+TInteger *Integer_new(int integer) {
+  TInteger *obj = new(&Integer, integer);
+  obj->integer = integer;
+
+  return obj;
+}
+
+void Inspect_TInteger_inspect(void *obj_ptr) {
+  TInteger *obj = (TInteger *) obj_ptr;
+  printf("TInteger: {integer: %d}\n", obj->integer);
+}
+
+
+InspectTraitImplementation InspectTIntegerImplementation = {
+  .inspect = &Inspect_TInteger_inspect
+};
+
+
+
 void initialize_trait_implementations(){
   Inspect.trait.implementations = HashMap_new(2);
   HashMap_insert(&Inspect.trait.implementations, &Char, &InspectTCharImplementation);
+  HashMap_insert(&Inspect.trait.implementations, &Integer, &InspectTIntegerImplementation);
 }
 
 void free_trait_implementations(){
@@ -96,14 +186,10 @@ int main(void) {
   initialize_trait_implementations();
 
   TChar *foo = Char.new('a');
-  /* TChar * foo = calloc(1, sizeof(TChar)); */
-  /* foo->typeref.typeref = &Char; */
-  /* foo->character = 'a'; */
-  /* InspectTCharImplementation.inspect(foo); */
-  /* void (*res)(void *) = HashMap_lookup(Inspect.trait.implementations, foo->type.typeref); */
-  /* res(foo); */
+  TInteger *bar = Integer.new(1234);
   Inspect.inspect(foo);
+  Inspect.inspect(bar);
   free(foo);
-
+  free(bar);
   free_trait_implementations();
 }
