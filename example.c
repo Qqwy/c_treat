7c12cf32f1e1570b494f3fce099375d42f39cc1d
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-02-18 21:48:09 +0100

    a fun macro ;D

--- example.c
+++ example.c
@@ -8,15 +8,30 @@
 
 #include "treat/types/char/char.h"
 #include "treat/types/integer/integer.h"
+#include "treat/with.macro"
+
+
 
 int main(void) {
   Treat.init();
 
-  TChar *foo = Char.new('a');
-  TInteger *bar = Integer.new(1234);
-  Inspect.inspect(foo);
-  Inspect.inspect(bar);
-  Char.destroy(foo);
-  Integer.destroy(bar);
+  /* TInteger *foo, *bar = Integer.new(123); */
 
+  T_with(TChar * baz, Char.new('b'), {
+    Inspect.inspect(baz);
+    Inspect.inspect(baz);
+
+    TChar *foo = Char.new('a');
+    TInteger *bar = Integer.new(1234);
+    Inspect.inspect(foo);
+    Inspect.inspect(bar);
+    Char.destroy(foo);
+    Integer.destroy(bar);
+
+
+
+    T_with(TInteger * my_int, Integer.new(1235), {
+        Inspect.inspect(my_int);
+    });
+  });
 }
